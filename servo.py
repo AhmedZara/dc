d4a3fe5e7377285c26d3b15618157d7f89ba7795
AhmedZara <zara586243@gmail.com>
2018-07-20 14:30:12 +0530

    servo added

--- servo.py
+++ servo.py
@@ -7,7 +7,7 @@ GPIO.setmode (GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)               
 
 p = GPIO.PWM(7,50)        
-p.start(7,5)                            
+p.start(7.5)                            
 
 try:
     while True:
